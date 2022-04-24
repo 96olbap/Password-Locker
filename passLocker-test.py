7e85236516bbdd0836a5e8f091d3f3c4ea870492
96olbap <alvinwanjala@gmail.com>
2022-04-24 23:19:01 +0300

    add test to check if we can save multiple users

--- passLocker-test.py
+++ passLocker-test.py
@@ -11,12 +11,36 @@ class TestPassLocker(unittest.TestCase):
         '''
         self.new_user = User('Weps', 'pass123') #create user object
 
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
         self.assertEqual(self.new_user.username, 'Weps')
         self.assertEqual(self.new_user.password,'pass123')
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_users(self):
+        '''
+        test_save_multiple_users to check if we can save multiple user
+        objects to the user_list
+        '''
+        self.new_user.save_user()
+        test_user = User('Test', 'test123')
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
 
 if __name__ == '__main__':
     unittest.main()
