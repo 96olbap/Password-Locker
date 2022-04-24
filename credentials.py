fe2f56cd3fc48efaf0658b04f2c5b653cb6d861b
96olbap <alvinwanjala@gmail.com>
2022-04-25 00:21:12 +0300

    add the class Credentials

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credentials:
+    '''
+    Class that generates instances of credentials
+    '''
+    def __init__(self, acc_name, acc_username, acc_password):
+        '''__init__ method that helps define properties to the class objects
+        '''
+        self.acc_name = acc_name
+        self.acc_username = acc_username
+        self.acc_password = acc_password
+        
