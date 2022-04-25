dbdc7871184c26ea8def23832956d325baeabed0
96olbap <alvinwanjala@gmail.com>
2022-04-25 10:45:17 +0300

    This commit will update the logic on run.py

--- run.py
+++ run.py
@@ -75,7 +75,7 @@ def main():
     print('\n')
 
     while True:
-        print("Use these short codes : ca - create a new account, lg - Log in to your account")
+        print("Use these short codes : cu - create a new account, lg - Log in to your account")
         short_code = input().lower()
         if short_code == 'cu':
             print("Create account")
@@ -88,7 +88,58 @@ def main():
             print('Account created successfully! Your accound details are as follows:')
             print("-"*10)
             print(f"Name: {username} \nPassword: {password}")
+            print(f"\nUse your details to log into your account")
+            print("\n \n")
+        elif short_code == 'lg':
+            print("Enter username...")
+            loginusername = input()
+            print("Enter your password:")
+            loginpassword = input()
+            if find_user(loginpassword):
+                print("\n")
+                print("Use these short codes : ca - create a another account, ve - View existing accounts")
+                print("-"*60)
+                choose = input().lower()
+                if choose == 'ca':
+                    print("Add your account credentials...")
+                    print("-"*25)
+                    accountUsername = loginusername
+                    print("Acount name: eg twitter, instagram, etc")
+                    accountName = input()
+                    print("\n")
+                    print("Use these short codes: g - Generate automatic password or cp - create your own new password")
+                    decision = input()
+                    if decision == 'g':
+                        characters = string.ascii_letters + string.digits
+                        accountPassword = "".join(choice(characters)for x in range(randint(6,16)))
+                        print(f"Password: {accountPassword}")
+                    elif decision == 'cp':
+                        print("Enter your password:")
+                        accountPassword = input()
+                    else:
+                        print("Kindly put in a valid choice")
+                    save_account(create_account_cred(accountName, accountUsername, accountPassword))
+                    print('\n')
+                    print(f" Account Name:{accountName} \nUsername:{accountUsername} \nPassword:{accountPassword}")
+                elif choose == 've':
+                    if find_account(accountUsername):
+                        print("Here is a list of your created accounts:")
+                        print("-"*25)
+                        for user in display_accounts():
+                            print(f"Account: {accountName} \nPassword: {accountPassword} \n\n")
+                    else :
+                        print("Invalid credentials!")
+                else:
+                    print("Kindly put in a valid choice")
+                    print("\n")
+            else:
+                print("Incorrect username or password, kindly try again! Thank you")
+                print("\n")
+        else:
+            print("Kindly put in a valid choice")
+            print("\n")
+
+
 
 if __name__ == '__main__':
-
     main()
