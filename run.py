e50a3742c1b864cf83b47e88c9d16e3fede091a4
96olbap <alvinwanjala@gmail.com>
2022-04-25 09:38:10 +0300

    update run.py to be able to create a new user

--- run.py
+++ run.py
@@ -39,3 +39,56 @@ def display_users():
     Function that returns all the saved users
     '''
     return User.display_users()
+
+def create_account_cred(accountName, accountUsername, accountPassword):
+    '''
+    Function that creates a new account's credentials
+    '''
+
+    new_account =  Credentials(acc-name, acc-username, acc-password)
+    return new_account
+
+def save_account(user):
+    '''
+    Function to save a user's account credentials
+    '''
+    user.save_credentials()
+
+def delete_account(user):
+    '''
+    Function to delete a user's account credentials
+    '''
+    user.delete_credentials()
+
+def find_account(username):
+    return Credentials.find_by_username(username)
+
+def display_accounts():
+    return Credentials.display_credentials()
+
+def main():
+
+    print("Hello Welcome to Password Locker. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use these short codes : ca - create a new account, lg - Log in to your account")
+        short_code = input().lower()
+        if short_code == 'cu':
+            print("Create account")
+            print("-"*10)
+            print ("Set username ....")
+            username = input()
+            print ("Set password ....")
+            password = input()
+            save_users(create_user(username, password))
+            print('Account created successfully! Your accound details are as follows:')
+            print("-"*10)
+            print(f"Name: {username} \nPassword: {password}")
+
+if __name__ == '__main__':
+
+    main()
